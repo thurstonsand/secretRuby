e91f8814be2cb36a93e1aeeea48d7af770c23fb3
Thurston Sandberg <thurstonsand@gmail.com>
2012-07-16 18:52:10 -0400

    added login

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  #Returns the header
+  def full_title(page_title)
+    base_title="SeedSwap"
+    if(page_title.empty?)
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 SecretRuby::Application.routes.draw do
+  get "login/home"
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
